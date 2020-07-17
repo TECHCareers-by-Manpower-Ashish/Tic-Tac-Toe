81abe47c6177b1fbc9422ac023acdbe49777cfc7
DeepAshish <Pashish@live.ca>
2020-07-17 10:25:12 -0600

    intial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+       
+int player1, player2;
+       char [,] arr = {{'1','2','3'},
+                      {'4','5','6'},
+                      {'7','8','9'}};
+static int player = 1;
+
+
+static void Introduction()
+{
+    Console.WriteLine("Welcome to Tic_Tac_Toe");
+    Console.WriteLine("Please press enter key for start Game");
+    starting v = Console.ReadLine();
+    Console.WriteLine("Game rules");
+    Console.WriteLine("Here two players can play: player 1 and player 2");
+    Console.WriteLine("Both players place tokens into a square on a 3x3 board");
+    Console.WriteLine("player 1 start first");
+    Console.WriteLine("when three horizontal lines formed, three horizontal lines formed,game end two possible dignal lines are formed,the board is full and no lines are formed.");
+    console.ReadLine();
+
+
+
+
+        static void Main(string[] args)
+        
+        {
+
+  
+
+
+ /*   static void Introduction()
+{
+    Console.WriteLine("Welcome to Tic_Tac_Toe");
+    Console.WriteLine("Please press enter key for start Game");
+    starting v = Console.ReadLine();
+    Console.WriteLine("Game rules");
+    Console.WriteLine("Here two players can play: player 1 and player 2");
+    Console.WriteLine("Both players place tokens into a square on a 3x3 board");
+    Console.WriteLine("player 1 start first");
+    Console.WriteLine("when three horizontal lines formed, three horizontal lines formed,game end two possible dignal lines are formed,the board is full and no lines are formed.");
+    console.ReadLine();
+}  */
+     
+
+
+        }
+    }
+}
+
+}
